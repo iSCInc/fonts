5391a3356a0e04e9bf358eed819bc43d48f1924c
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2014-10-31 15:19:07 +0100

    Create check-sync.sh

---
+++ check-sync.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Refreshing remote"
+git pull
+echo "Refreshing submodules"
+git submodule update --init
+
+echo "Comparing HEAD and origin/master of each repository..."
+echo "------------------------------------------------------"
+git submodule --quiet foreach 'test "$(git rev-list HEAD..origin/master --count)" = "0" || echo "ERROR! $path is lagging behind."'
+echo "------------------------------------------------------"
+
+echo "Done! Any ERROR! up this line should be reported in bugzilla"
